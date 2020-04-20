27b843b3b2275c1441bf3a555ed5ea82d6139529
pauladarias <pauladarias@gmail.com>
2020-04-20 16:02:55 +0100

    initialize current balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
+    subject(:oystercard) {described_class.new}
+
+    it "oyster card has 0 as default value" do 
+        expect(oystercard.balance).to eq(0)
+    end 
 
 end
